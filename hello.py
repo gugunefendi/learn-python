1e3b9540762ffe3ebda477d36587e7526b56e25c
Gunawan Efendi <gunawanefendi32@gmail.com>
2024-11-27 14:21:56 +0700

    Hello world

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Welcome to Python App")
+print("---------------------")
+
+name = input("what your name? ")
+age = input("How old are you? ")
+
+print("Hello", name)
+print("You are", age, "years old")
