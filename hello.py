89549a6644f909bc70833ab132eee2fe92a72471
Gunawan Efendi <gunawanefendi32@gmail.com>
2024-11-27 21:25:31 +0700

    single quote and double quote

--- hello.py
+++ hello.py
@@ -14,5 +14,13 @@ name = input("what your name? ")
 age = input("How old are you? ")
 
 # Say hello to user
-print("Hello", name)
-print("You are", age, "years old")
+print("Hello", name, "you are", age, "years old")
+
+""""
+You can use single quote or double quote, but you should consistent
+When we want print double quote inside a string, example: 'Hello "friend"'
+But is better if you add backslash in string start and string end, example: 'Hello \"friend\"'
+"""
+
+# Add backslash in string start and string end
+print(name, 'is my \"brother\"')
