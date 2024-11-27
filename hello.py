6610d265f205a61be9522c395a81cbdb0b99a4a5
Gunawan Efendi <gunawanefendi32@gmail.com>
2024-11-27 20:57:41 +0700

    comment

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
+"""
+This is multi line comment
+"""
+
+#
+# these are also multi line comment
+#
+
 print("Welcome to Python App")
 print("---------------------")
 
+# Ask user for their name and age
 name = input("what your name? ")
 age = input("How old are you? ")
 
+# Say hello to user
 print("Hello", name)
 print("You are", age, "years old")
