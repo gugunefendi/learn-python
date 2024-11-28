fff8dc0a7861ea7983844046e14f723a71185f27
Gunawan Efendi <gunawanefendi32@gmail.com>
2024-11-28 20:13:50 +0700

    int

---
+++ calculator.py
@@ -0,0 +1,17 @@
+x = 1
+y = 2
+
+z = x + y
+print(z)
+
+firstNumber = input("Input first number: ")
+secondNumber = input("Input second number: ")
+
+sum = firstNumber + secondNumber
+
+print(sum) # this will print string
+
+# add int() for convert string to int
+repairSum = int(firstNumber) + int(secondNumber)
+
+print(repairSum)
