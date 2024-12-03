aa8fa7b407f266780a5c829d0f766d59dd215d12
Gunawan Efendi <gunawanefendi32@gmail.com>
2024-12-03 17:09:13 +0700

    return values

---
+++ return_values.py
@@ -0,0 +1,9 @@
+def greet(input):
+    if "hello" in input:
+        return "hello, there"
+    else:
+        return "I am not sure what you mean"
+    
+greeting = greet("hello world")
+
+print(greeting + "Gunawan")
