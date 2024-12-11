e343405bfe72a67ab580b5ff095ac6f4146211c5
Gunawan Efendi <gunawanefendi32@gmail.com>
2024-12-11 17:39:49 +0700

    list

---
+++ list.py
@@ -0,0 +1,11 @@
+students = ["Adi", "Budi", "Cici"]
+
+print(students[0])
+print(students[1])
+print(students[2])
+
+for student in students:
+    print(student)
+
+for i in range(len(students)):
+    print("Number: ", i + 1, "Name", students[i])
